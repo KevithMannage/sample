f03be1c4ba8336b233d73527f0816a5ade674e49
induwrawithanage <induwaravithanage02@gmail.com>
2025-04-02 18:11:50 +0530

    Implement pages

---
+++ frontend/src/pages/ContactUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../navbar';
+const AboutUs = () => {
+  return (
+    <div>
+        <Navbar/>
+      <h1>Welcome to the AboutUs!</h1>
+      <p>This is your main AboutUs page.</p>
+    </div>
+  );
+};
+
+export default AboutUs;
