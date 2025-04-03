1ebe65a847584b99e1ad69a63b79f25d9ccf5830
kevith mannage <m.mannage@gmail.com>
2025-04-03 18:06:41 +0530

    add a symbol

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,13 @@ import Navbar from './navbar';
 // Import your page components
 import LoginPage from './Login';
 import Dashboard from './Dashboard';
+import AboutUs from './pages/AboutUs';
+ import ContactUs from './pages/ContactUs';
+ import Discussion from './pages/Discussion';
+ import Messages from './pages/Message';
+ import NewDiscussion from './pages/NewDiscussion';
+ import Profile from './pages/Profile';
+ import Home from './pages/Home';
 import { FaRegFileCode } from 'react-icons/fa';
 const App = () => {
   return (
@@ -13,9 +20,17 @@ const App = () => {
         <main className="main-content">
           <Routes>
           <Route path="/" element={<LoginPage/>} />
-            <Route path="/login" element={<LoginPage/>} />
-            <Route path="/dashboard" element={<Dashboard/>} />
-           
+             <Route path="/login" element={<LoginPage/>} />
+             <Route path="/dashboard" element={<Dashboard/>} />
+            
+             <Route path="/aboutus" element={<AboutUs/>} />
+             <Route path="/contactus" element={<ContactUs/>} />  
+             <Route path="/discussion" element={<Discussion/>} />
+             <Route path="/messages" element={<Messages/>} />
+             <Route path="/newdiscussion" element={<NewDiscussion/>} />
+             <Route path="/profile" element={<Profile/>} />
+             <Route path="/home" element={<Home/>} />
+ 
           </Routes>
         </main>
       </div>
