e20b87378bf95f6df809610f22756f88f5f817ed
kevith mannage <m.mannage@gmail.com>
2025-04-08 18:58:36 +0530

    update notification

--- frontend/src/hooks/checkNotification.js
+++ frontend/src/hooks/checkNotification.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const checkNotification = async () => {
   const user_id = localStorage.getItem('userid'); // Get user_id from local storage
-
+  const backendUrl= "https://devthonbackend-production.up.railway.app";
   if (!user_id) {
     console.warn('User ID not found in local storage.');
     return;
@@ -10,7 +10,7 @@ const checkNotification = async () => {
 
   try {
     // Call the backend API to get subscribed discussion IDs
-    const response = await axios.get(`http://localhost:3000/notifications/${user_id}`);
+    const response = await axios.get(`${backendUrl}/notifications/${user_id}`);
 
     if (response.status === 200) {
       const { discussionIds } = response.data;
