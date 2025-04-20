aa3ba03915d4d652d4b1eef51ccb9f57b1fe332f
UmeshaJayakody <umesha.22@cse.mrt.ac.lk>
2025-04-20 12:16:31 +0530

    Temporarily disable Google login redirect and add alert for upcoming feature

--- frontend/src/Professionallogin.jsx
+++ frontend/src/Professionallogin.jsx
@@ -132,7 +132,8 @@ const ProfessionalLoginPage = () => {
 
 
   const handleGoogleLogin = () => {
-    window.location.href = 'https://devthonbackend-production.up.railway.app/auth/google'; // Replace with your backend URL
+    alert('Comming Soon!');
+    //window.location.href = 'https://devthonbackend-production.up.railway.app/auth/google'; // Replace with your backend URL
   };
 
   const handleSubmit = async (e) => {
